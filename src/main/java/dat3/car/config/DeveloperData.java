5782152cc5c40b63591065999f83b878d5e19997
MeH2607 <moha898h@stud.kea.dk>
2023-08-26 10:55:12 +0200

    wrote car class and filled out test data for cars

--- src/main/java/dat3/car/config/DeveloperData.java
+++ src/main/java/dat3/car/config/DeveloperData.java
@@ -1,11 +1,42 @@
 package dat3.car.config;
 
+import dat3.car.entity.Car;
+import dat3.car.repository.CarRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeveloperData implements ApplicationRunner {
+    CarRepository carRepository;
+    public DeveloperData(CarRepository carRepository){
+        this.carRepository = carRepository;
+    }
     @Override
     public void run(ApplicationArguments args) throws Exception {
+    List<Car> carList = new ArrayList<>();
 
+        carList.add(new Car("Toyota", "Corolla", 50.0, 10));
+        carList.add(new Car("Honda", "Civic", 55.0, 8));
+        carList.add(new Car("Ford", "Focus", 48.0, 12));
+        carList.add(new Car("Chevrolet", "Malibu", 60.0, 5));
+        carList.add(new Car("Nissan", "Altima", 52.0, 15));
+        carList.add(new Car("Volkswagen", "Jetta", 45.0, 10));
+        carList.add(new Car("Hyundai", "Elantra", 47.0, 10));
+        carList.add(new Car("Kia", "Forte", 50.0, 10));
+        carList.add(new Car("Subaru", "Impreza", 55.0, 8));
+        carList.add(new Car("Mazda", "3", 53.0, 12));
+        carList.add(new Car("Audi", "A3", 70.0, 5));
+        carList.add(new Car("BMW", "3 Series", 75.0, 15));
+        carList.add(new Car("Mercedes-Benz", "C-Class", 80.0, 10));
+        carList.add(new Car("Lexus", "IS", 65.0, 10));
+        carList.add(new Car("Volvo", "S60", 68.0, 10));
+        carList.add(new Car("Tesla", "Model 3", 90.0, 8));
+        carList.add(new Car("Porsche", "Panamera", 110.0, 12));
+        carList.add(new Car("Jaguar", "XE", 100.0, 5));
+        carList.add(new Car("Infiniti", "Q50", 75.0, 15));
+        carList.add(new Car("Land Rover", "Range Rover", 150.0, 10));
+        carRepository.saveAll(carList);
     }
 }
---
+++ src/main/java/dat3/car/entity/Car.java
@@ -0,0 +1,36 @@
+package dat3.car.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_ID", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "car_brand", unique = true, length = 50, nullable = false)
+    private String brand;
+    @Column(name = "car_model", unique = true, length = 60, nullable = false)
+    private String model;
+    @Column(name = "rental_price_day")
+    private double pricePrDay;
+    @Column(name = "max_discount")
+    private int bestDiscount;
+
+    public Car(String brand, String model, double pricePrDay, int bestDiscount) {
+        this.brand = brand;
+        this.model = model;
+        this.pricePrDay = pricePrDay;
+        this.bestDiscount = bestDiscount;
+    }
+}
